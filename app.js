7fcf15f26722d17b34b5d7a1a03ed5d8d1d76be1
johnestes4 <johnestes4@gmail.com>
2016-08-19 09:36:38 -0400

    i used copy-paste for evil and didn't realize this doesn't need any router at all

--- app.js
+++ app.js
@@ -2,21 +2,5 @@
 
 (function(){
   angular
-  .module("compliments", [
-    "ui.router"
-  ])
-  .config([
-    "$stateProvider",
-    RouterFunction
-  ]);
-// only one state is really needed for a one-page app
-  function RouterFunction($stateProvider){
-    $stateProvider
-    .state("index", {
-      url: "",
-      templateUrl: "js/compliments/index.html",
-      controller: "complimentsController",
-      controllerAs: "complimentsViewModel"
-    });
-  }
+  .module("compliments", [])
 }());
